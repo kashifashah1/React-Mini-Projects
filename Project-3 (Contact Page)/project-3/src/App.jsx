a7b2a8d1fa559d3b2c2c9e388507d2652be7d4a2
kashifashah1 <kashifa.shah24@gmail.com>
2025-08-03 19:21:09 +0530

    Project added

---
+++ Project-3 (Contact Page)/project-3/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ContactHeader from './components/ContactHeader/ContactHeader'
+import Navbar from './components/Navigation/Navbar'
+
+function App() {
+
+  return <div>
+    <Navbar/>
+    <ContactHeader/>
+  </div>
+}
+
+export default App
---
+++ Project-3 (Contact Page)/project-3/src/components/Navigation/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "./Navigation.module.css"
+
+const Navbar = () => {
+  return (
+    <nav className={` container ${styles.navigation} `}>
+        <div className='logo'>
+            <img src="/images/Frame 2 1.png" alt="do some coding logo" />
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+      
+    </nav>
+  )
+}
+
+export default Navbar
